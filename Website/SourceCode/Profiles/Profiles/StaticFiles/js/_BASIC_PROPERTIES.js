32681a7a10ed6afe5197c28f0810a513e7547a07
Carl Woolf <carl_woolf@hms.harvard.edu>
2024-04-25 14:29:28 -0400

    more granular basic url properties

--- Website/SourceCode/Profiles/Profiles/StaticFiles/js/_BASIC_PROPERTIES.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/js/_BASIC_PROPERTIES.js
@@ -1,3 +1,8 @@
 let gBasic = {
-    jsUrlRoot: "/StaticFiles/"
+    jsUrlRoot: "/StaticFiles/",
 }
+gBasic.jsBrandingImageFiles =   `${jsUrlRoot}branding`;
+gBasic.jsCommonImageFiles =     `${jsUrlRoot}common`;
+gBasic.jsPagingImageFiles =     `${jsUrlRoot}paging`;
+gBasic.jsPersonImageFiles =     `${jsUrlRoot}person`;
+gBasic.jsSearchImageFiles =     `${jsUrlRoot}search`;

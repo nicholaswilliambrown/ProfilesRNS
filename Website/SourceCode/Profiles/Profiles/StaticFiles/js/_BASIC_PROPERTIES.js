404a504ca65059428ad49699412d2b8951f5bce4
Carl Woolf <carl_woolf@hms.harvard.edu>
2024-04-26 12:48:23 -0400

    one more tweak gBasic derivative values

--- Website/SourceCode/Profiles/Profiles/StaticFiles/js/_BASIC_PROPERTIES.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/js/_BASIC_PROPERTIES.js
@@ -1,8 +1,8 @@
 let gBasic = {
     jsUrlRoot: "/StaticFiles/",
 }
-gBasic.jsBrandingImageFiles =   `${gBasic.jsUrlRoot}img/branding`;
-gBasic.jsCommonImageFiles =     `${gBasic.jsUrlRoot}img/common`;
-gBasic.jsPagingImageFiles =     `${gBasic.jsUrlRoot}img/paging`;
-gBasic.jsPersonImageFiles =     `${gBasic.jsUrlRoot}img/person`;
-gBasic.jsSearchImageFiles =     `${gBasic.jsUrlRoot}img/search`;
+gBasic.jsBrandingImageFiles =   `${gBasic.jsUrlRoot}img/branding/`;
+gBasic.jsCommonImageFiles =     `${gBasic.jsUrlRoot}img/common/`;
+gBasic.jsPagingImageFiles =     `${gBasic.jsUrlRoot}img/paging/`;
+gBasic.jsPersonImageFiles =     `${gBasic.jsUrlRoot}img/person/`;
+gBasic.jsSearchImageFiles =     `${gBasic.jsUrlRoot}img/search/`;

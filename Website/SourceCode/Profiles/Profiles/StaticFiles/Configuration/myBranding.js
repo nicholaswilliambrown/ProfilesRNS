1ec8717d19b6ffa63de20a0e296eb680078a8c0c
Carl Woolf <carl_woolf@hms.harvard.edu>
2025-07-17 12:52:47 -0400

    reinstate standard OpenSource branding

--- Website/SourceCode/Profiles/Profiles/StaticFiles/Configuration/myBranding.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/Configuration/myBranding.js
@@ -1,39 +1,23 @@
+
 let gBrandingConstants = {};
 
 // gBrandingConstants.staticFiles helps locate all the other site-specific resources
-gBrandingConstants.staticRoot = "/StaticFiles/";
+gBrandingConstants.staticRoot = g.staticRoot;
 
+async function emitBrandingHeader(targetId) {
+
+    let header = $(`<div id="brandingBanner" class="mb-3"></div>`);
+    $(`#${targetId}`).prepend(header);
+
+    let bannerDiv = $('<div class="d-flex justify-content-center headerBanner w-100"></div>');
+    await header.append(bannerDiv);
+}
+async function emitBrandingFooter(targetId) {
+    let brandingFooter = await $('#brandingBanner').clone();
+    brandingFooter.attr('id', "brandingFooter");
+    brandingFooter.hide(); // show once rest of page is loaded!
+    await $(`#${targetId}`).append(brandingFooter);
+}
 async function setupHeadIncludesAndTabTitle() {
     setTabTitleAndOrFavicon();
-}
-async function emitBrandingHeader() {
-
-    $('body').prepend(`<div id="brandingBanner" class="d-flex justify-content-center headerFooter p-2"></div>`);
-
-    let getHeaderContent = () => { return $(`<div>Foo University<br/>We lower the bar</div>`); }
-
-    // versions for small and large
-    let largeBannerDiv = $(`<div class="w-75 ${gCommon.hideXsSmallShowOthers}"></div>`);
-    $('#brandingBanner').append(largeBannerDiv);
-    largeBannerDiv.append(getHeaderContent());
-
-    let smallBannerDiv = $(`<div class="w-75 ${gCommon.showXsSmallHideOthers}"></div>`);
-    $('#brandingBanner').append(smallBannerDiv);
-    smallBannerDiv.append(getHeaderContent());
-}
-async function emitBrandingFooter() {
-    let brandingFooter = $(`<div id="brandingFooter" 
-        class="d-flex justify-content-center headerFooter p-2"></div>`);
-    $('body').append(brandingFooter);
-
-    let getFooterContent = () => { return $(`<div class="small">Foo University<br/>Try not to trip on the bar</div>`); }
-
-    // versions for small and large
-    let largeFooterDiv = $(`<div class="w-75 ${gCommon.hideXsSmallShowOthers}"></div>`);
-    brandingFooter.append(largeFooterDiv);
-    largeFooterDiv.append(getFooterContent());
-
-    let smallFooterDiv = $(`<div class="w-75 ${gCommon.showXsSmallHideOthers}"></div>`);
-    brandingFooter.append(smallFooterDiv);
-    smallFooterDiv.append(getFooterContent());
 }

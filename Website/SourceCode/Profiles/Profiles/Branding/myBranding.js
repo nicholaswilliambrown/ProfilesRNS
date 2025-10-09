9cc59aae19457407b8e16b26f0f1afca603d2868
Carl Woolf <carl_woolf@hms.harvard.edu>
2025-10-09 09:52:24 -0400

    fixed-dimensions for header image. defer mobile version (narrow width) for now

--- Website/SourceCode/Profiles/Profiles/Branding/myBranding.js
+++ Website/SourceCode/Profiles/Profiles/Branding/myBranding.js
@@ -9,8 +9,12 @@ async function emitBrandingHeader(targetId) {
     let header = $(`<div id="brandingBanner" class="mb-3"></div>`);
     $(`#${targetId}`).prepend(header);
 
-    let bannerDiv = $('<div class="d-flex justify-content-center headerBanner w-100"></div>');
+    let bannerDiv = $('<div class="headerBanner w-100"></div>');
     await header.append(bannerDiv);
+
+    // potential for wide / narrow responsive alternates
+    let imageDivWide = $('<div id="imageDivWide" class="d-flex justify-content-center"></div>');
+    bannerDiv.append(imageDivWide);
 }
 async function emitBrandingFooter(targetId) {
     let brandingFooter = $('<div id="brandingFooter"' +
@@ -20,7 +24,6 @@ async function emitBrandingFooter(targetId) {
         'The Harvard Clinical and Translational Science Center from the ' +
         'National Center for Advancing Translational Sciences and support from ' +
         'Harvard University and its affiliated academic healthcare centers.</div>');
-    brandingFooter.append($('<img source="../StaticFiles/img/branding/_OpenSourceBanner.png" id="bannerImage"></img>'))
     brandingFooter.hide(); // show once rest of page is loaded!
     await $(`#${targetId}`).append(brandingFooter);
 

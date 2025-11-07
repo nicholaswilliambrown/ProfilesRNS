cbb8e57386e2517e0afaa5e8c9772f729a31fffd
Carl Woolf <carl_woolf@hms.harvard.edu>
2025-11-07 15:37:50 -0500

    fix bibliometrics invocation-bug

--- Database/000-fresh-install/ProfilesRNS/Database/setup.bat
+++ Database/000-fresh-install/ProfilesRNS/Database/setup.bat
@@ -1,8 +1,8 @@
 @echo off
 echo.
 echo Usage: %0 ^(for this usage message^) ^|
-echo        %0 ^(In this order, any or all of:^)
-echo             initial pubMedDisambiguation pubMedXML bibliometrics funding rdf jobGroups
+echo        %0 In the following order, any or all of:
+echo             initial PubMedDisambiguation_GetPubs GetPubMedXML bibliometrics funding rdf jobGroups
 echo         NB: sanity ^(for DB sanity check^) can occur anywhere ^(and multiple times^) in the above sequence
 echo.
 
@@ -14,53 +14,39 @@ set DB_NAME=ProfilesRNS
 set SA_PW=DB_Password
 
 for %%x in (%*) do (
-  call :doStep %%x
+  call :doTask %%x
 )
-exit /b 0
-echo should not get here
+goto :bye
 
-:doStep
+:doTask
 echo.
-set step=%~1
-echo --------- arg is %step%
-if /I %step% == pubMedDisambiguation (
-   set step=PubMedDisambiguation_GetPubs
-   call :doJob %step%
-   goto :bye
-)
-if /I %step% == pubMedXML (
-   set step=GetPubMedXML
-   call :doJob %step%
-   goto :bye
-)
-if /I %step% == funding (
-   call :doJob %step%
-   goto :bye
-)
-if /I %step% == rdf (
-   call :doJob %step%
-   goto :bye
-)
-
-call :%step%
-    goto :bye
-
-:bye
-cd %INSTALL_PATH%
-exit /b
+set task=%~1
+echo ============= Start task: %task% at =============  %date% %time%
+set jobTask=no
+FOR %%G IN ("PubMedDisambiguation_GetPubs", "GetPubMedXML", "bibliometrics", "funding") DO (IF /I "x%1" == "x%%~G" set jobTask=yes)
+IF "x%jobTask%" == "xyes" ( call :doJob %1 ) ELSE ( call :%1 )
+echo ============= Done task: %task% at =============  %date% %time%
+goto :bye
 
 :doJob
 cd %DOT_NET_PERSON_DATA_BIN_PATH%
 echo --------- Prerequisite: ProfilesRNS_CallPRNSWebservice.exe has been built
-echo --------- ProfilesRNS_CallPRNSWebservice.exe -server %SERVER% -database %DB_NAME% -username sa -password %SA_PW% -job %step%
-ProfilesRNS_CallPRNSWebservice.exe -server %SERVER% -database %DB_NAME% -username sa -password %SA_PW% -job %step%
-   goto :bye
+echo --------- ProfilesRNS_CallPRNSWebservice.exe -server %SERVER% -database %DB_NAME% -username sa -password %SA_PW% -job %task%
+ProfilesRNS_CallPRNSWebservice.exe -server %SERVER% -database %DB_NAME% -username sa -password %SA_PW% -job %task%
+echo --------- Done with job: %task%
+goto :bye
 
 :sanity
 echo.
 echo ----------------------------------Sanity check
+echo --------------------select count(*)  from [RDF.Stage].[Log.DataMap]
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "select count(*) from [RDF.Stage].[Log.DataMap]"                                                        | findstr /R /C:"[0-9]
+echo --------------------SELECT * FROM [Framework.].[-P %SA_PW% -d %DB_NAME% -Q "SELECT * FROM [Framework.].[Job] o WHERE  o.Status = 'PROCESSING'"
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SELECT * FROM [Framework.].[Job] o WHERE  o.Status = 'PROCESSING'"                                     | findstr /R /C:"[0-9]  
 echo --------------------SELECT job from [Profile.Import].[PRNSWebservice.Options]
-sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SELECT job from [Profile.Import].[PRNSWebservice.Options]"                                             | findstr /V /R /C:"[0-9]"
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SELECT job from [Profile.Import].[PRNSWebservice.Options]"
+echo --------------------SELECT JobGroup,Name FROM [Framework.].JobGroup
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SELECT JobGroup,Name FROM [Framework.].JobGroup" 
 echo --------------------Select count(*) from [Profile.Import].[Person]
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SET NOCOUNT ON; Select count(*) from [profile.import].[Person]"                                        | findstr /R /C:"[0-9]"
 echo --------------------Select count(*) from [Profile.Import].[PersonAffiliation]
@@ -76,7 +62,7 @@ sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SET NOCOUNT ON; Select coun
 echo --------------------Select count(*) from [Profile.Data].[Publication.Pubmed.Allxml]
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "SET NOCOUNT ON; Select count(*) from [profile.data].[publication.pubmed.allxml]"                       | findstr /R /C:"[0-9]"
 echo.
-   goto :bye
+goto :bye
 
 :initial
 %INSTALL_DRIVE%
@@ -110,21 +96,40 @@ echo.
 echo ----------------------------------Load funding disambiguation strings
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -i fundingDisambiguationSample.sql
 echo.
-   goto :bye
+goto :bye
 
 :rdf
 echo ----------------------------------RDF conversion
-sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -i ProfilesRNS_DataLoad_Part3.sql
-   goto :bye
+:: ProfilesRNS_DataLoad_Part3.sql took too long with no indication of progress
+echo EXEC [Framework.].[RunJobGroup] @JobGroup = 10
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 10"
+echo EXEC [Framework.].[RunJobGroup] @JobGroup = 7
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 7"
+echo EXEC [Framework.].[RunJobGroup] @JobGroup = 8
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 8"
+echo EXEC [Framework.].[RunJobGroup] @JobGroup = 9
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 9"
+echo EXEC [Framework.].[RunJobGroup] @JobGroup = 3
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 3"
+echo EXEC [Profile.Cache].[Concept.UpdatePreferredPath]
+sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Profile.Cache].[Concept.UpdatePreferredPath]"
+goto :bye
 
 :jobGroups
 echo ----------------------------------Run JobGroups
+echo sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 4"
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 4"
+echo sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 5"
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 5"
+echo sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 6"
 sqlcmd -S %SERVER% -U sa -P %SA_PW% -d %DB_NAME% -Q "EXEC [Framework.].[RunJobGroup] @JobGroup = 6"
-echo EXEC [Framework.].[RunJobGroup] @JobGroup = 3
+
 echo.
-   goto :bye
+goto :bye
 
 
+
+:bye
+cd %INSTALL_PATH%
+exit /b 0 :: 0 may let it try subsequent task even if one fails
 

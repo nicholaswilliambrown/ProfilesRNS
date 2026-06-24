a47a186a9d283b37d891f5f30f418b2ac4bb5b52
Carl Woolf <carl_woolf@hms.harvard.edu>
2026-06-24 15:58:12 -0400

    blurb is handling 0, 1 or more persons . lists page override the 'clear' from topMenu

--- Website/SourceCode/Profiles/Profiles/StaticFiles/js/lists/listsPage.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/js/lists/listsPage.js
@@ -4,7 +4,7 @@ let gLists = {};
 gLists.manage = {
     setup: () => {
         console.log('manage');
-        parseLists($('#manageContent'), gLists.manage.people);
+        parseLists(gLists.manage.people);
     }
 };
 
@@ -28,7 +28,10 @@ async function setupListsPage() {
     $('.nav-item').on('click', adjustTab);
 
     gLists.manage.setup();
+
+    specialHandling();
 }
+
 function adjustTab(e) {
     let ariaCurr = 'aria-current';
     let tabs = $('.mainTabItem').find('.tab');
@@ -56,27 +59,40 @@ function adjustTab(e) {
     console.log('setup is: ', gLists[tabFlavor].setup);
     gLists[tabFlavor].setup();
 }
-function parseLists(target, people) {
 
-    let colSpecs0 = [
-        newColumnSpec(`${gCommon.cols3}`, 'Person'),
-        newColumnSpec(`${gCommon.cols3}`, 'Department'),
-        newColumnSpec(`${gCommon.cols3}`, 'Institution'),
-        newColumnSpec(`${gCommon.cols3}`, 'Faculty Rank'),
-    ];
-    makeRowWithColumns(target, 'ListHeader', colSpecs0);
+function parseLists(people) {
+    let numPeople = people.length;
+    if ( ! numPeople) {
+        $('#noPeople').removeClass('d-none');
+    } else {
+        $('#somePeople').removeClass('d-none');
 
-    for (let i = 0; i < people.length; i++) {
-        let peep = people[i];
+        if (numPeople != 1) { // == 1 is default html
+            let currentNumText = `are currently <span class="redBold">${numPeople}</span> people`;
+            let allPeopleShownText = `all ${numPeople} people shown`
 
-        let colSpecs = [
-            newColumnSpec(`${gCommon.cols3}`, peep.DisplayName),
-            newColumnSpec(`${gCommon.cols3}`, peep.DepartmentName),
-            newColumnSpec(`${gCommon.cols3}`, peep.InstitutionName),
-            newColumnSpec(`${gCommon.cols3}`, peep.FacultyRank),
-        ];
+            $('#currentNum').html(currentNumText);
+            $('#allPeopleShown').html(allPeopleShownText);
+        }
 
-        let id = 'person' + i;
-        makeRowWithColumns(target, id, colSpecs);
+        let target = $('#somePeople');
+        let colSpecs0 = [newColumnSpec(`${gCommon.cols3}`, 'Person'), newColumnSpec(`${gCommon.cols3}`, 'Department'), newColumnSpec(`${gCommon.cols3}`, 'Institution'), newColumnSpec(`${gCommon.cols3}`, 'Faculty Rank'),];
+        makeRowWithColumns(target, 'ListHeader', colSpecs0);
+
+        for (let i = 0; i < people.length; i++) {
+            let peep = people[i];
+
+            let colSpecs = [newColumnSpec(`${gCommon.cols3}`, peep.DisplayName), newColumnSpec(`${gCommon.cols3}`, peep.DepartmentName), newColumnSpec(`${gCommon.cols3}`, peep.InstitutionName), newColumnSpec(`${gCommon.cols3}`, peep.FacultyRank),];
+
+            let id = 'person' + i;
+            makeRowWithColumns(target, id, colSpecs);
+        }
     }
+}
+function specialHandling() {
+    $("#deleteAllFromListA").off().on("click", listsPageDeleteAll);
+}
+function listsPageDeleteAll() {
+    // window.location.refresh();
+    return false;
 }

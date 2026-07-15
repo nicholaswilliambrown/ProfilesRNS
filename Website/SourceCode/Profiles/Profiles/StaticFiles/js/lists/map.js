7157d7732006de84247f9b3e4c3646211460b7d7
Carl Woolf <carl_woolf@hms.harvard.edu>
2026-07-15 13:50:22 -0400

    small progress on maps

--- Website/SourceCode/Profiles/Profiles/StaticFiles/js/lists/map.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/js/lists/map.js
@@ -11,13 +11,24 @@ gLists.map = {
     }
 };
 
-function parseMapTabData(people) {
+async function parseMapTabData(people) {
     let target = $('#mapContent');
-    target.append($("<div>Possible map</div>"));
     if (people.length == 0) {
         noPeopleOnList(target);
     }
     else {
         target.append("<div>Here comes the map</div>");
+
+        let firstNodeId = people[0].NodeID;
+        let predicateForListMap = 963;
+        let url = `${g.profilesRootURL}/ProfileJsonSvc.aspx/getdata?s=${firstNodeId}&p=${predicateForListMap}&t=map`;
+        await jQuery.getJSON(jsonURL, function (json2) {
+            for (let j2=0; j2<json2.length; j2++) {
+                let jsonJ2 = json2[j2];
+                jsonTmp.push(jsonJ2)
+            }
+            g.pageJSON = jsonTmp;
+        });
+
     }
 }

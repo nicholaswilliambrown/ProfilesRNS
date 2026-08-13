19c193c4dccbc04a2e72452ae531686c5f2b781c
Carl Woolf <carl_woolf@hms.harvard.edu>
2026-08-13 15:39:56 -0400

    dynamically rename twitter in the front end

--- Website/SourceCode/Profiles/Profiles/StaticFiles/js/common/parsers/twitterParse.js
+++ Website/SourceCode/Profiles/Profiles/StaticFiles/js/common/parsers/twitterParse.js
@@ -1,14 +1,15 @@
 function twitterParser(json, moduleTitle, miscInfo, explicitTarget) {
-    let innerPayloadDiv = getTargetUntentavizeIfSo(moduleTitle, explicitTarget);
+    let resultDiv = $('<div></div>');
 
     for (let i=0; i<json.length; i++) {
         let elt = json[i];
         let name = elt.data;
-        let url = `https://twitter.com/${name}`;
+        let url = `https://x.com/${name}`;
 
         let p = $(`<p></p>`);
         p.append($(`<a class="link-ish" href="${url}">Tweets by ${name}</a>`));
 
-        innerPayloadDiv.append(p);
+        resultDiv.append(p);
     }
+    return resultDiv;
 }
